8e1bb64ffb17ed2fad9951ba262fcce35fed07ac
andrewpolar <61638749+andrewpolar@users.noreply.github.com>
2025-02-01 10:00:13 -0500

    Update KAN.cpp

--- KAN/KAN.cpp
+++ KAN/KAN.cpp
@@ -21,6 +21,65 @@
 #include "Helper.h"
 #include "Layer.h"
 
+//determinants
+std::unique_ptr<std::unique_ptr<double[]>[]> GenerateInput(int nRecords, int nFeatures, double min, double max) {
+	auto x = std::make_unique<std::unique_ptr<double[]>[]>(nRecords);
+	for (int i = 0; i < nRecords; ++i) {
+		x[i] = std::make_unique<double[]>(nFeatures);
+		for (int j = 0; j < nFeatures; ++j) {
+			x[i][j] = static_cast<double>((rand() % 10000) / 10000.0);
+			x[i][j] *= (max - min);
+			x[i][j] += min;
+		}
+	}
+	return x;
+}
+
+double determinant(const std::vector<std::vector<double>>& matrix) {
+	int n = (int)matrix.size();
+	if (n == 1) {
+		return matrix[0][0];
+	}
+	if (n == 2) {
+		return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+	}
+	double det = 0.0;
+	for (int col = 0; col < n; ++col) {
+		std::vector<std::vector<double>> subMatrix(n - 1, std::vector<double>(n - 1));
+		for (int i = 1; i < n; ++i) {
+			int subCol = 0;
+			for (int j = 0; j < n; ++j) {
+				if (j == col) continue;
+				subMatrix[i - 1][subCol++] = matrix[i][j];
+			}
+		}
+		det += (col % 2 == 0 ? 1 : -1) * matrix[0][col] * determinant(subMatrix);
+	}
+	return det;
+}
+
+double ComputeDeterminant(std::unique_ptr<double[]>& input, int N) {
+	std::vector<std::vector<double>> matrix(N, std::vector<double>(N, 0.0));
+	int cnt = 0;
+	for (int i = 0; i < N; ++i) {
+		for (int j = 0; j < N; ++j) {
+			matrix[i][j] = input[cnt++];
+		}
+	}
+	return determinant(matrix);
+}
+
+std::unique_ptr<double[]> ComputeDeterminantTarget(const std::unique_ptr<std::unique_ptr<double[]>[]>& x, int nMatrixSize, int nRecords) {
+	auto target = std::make_unique<double[]>(nRecords);
+	int counter = 0;
+	while (true) {
+		target[counter] = ComputeDeterminant(x[counter], nMatrixSize);
+		if (++counter >= nRecords) break;
+	}
+	return target;
+}
+//end
+
 std::unique_ptr<std::unique_ptr<double[]>[]> MakeRandomMatrix(int rows, int cols, double min, double max) {
 	std::unique_ptr<std::unique_ptr<double[]>[]> matrix;
 	matrix = std::make_unique<std::unique_ptr<double[]>[]>(rows);
@@ -44,7 +103,101 @@ std::unique_ptr<double[]> ComputeAreas(std::unique_ptr<std::unique_ptr<double[]>
 	return u;
 }
 
+void Determinant_4_4_3Layers() {
+	printf("Target is determinants of random 4*4 matrices, model is 3 layer KAN, metric is Pearson\n");
+	//generate data
+	int nTrainingRecords = 100000;
+	int nValidationRecords = 20000;
+	int nMatrixSize = 4;
+	int nFeatures = nMatrixSize * nMatrixSize;
+	double min = 0.0;
+	double max = 10.0;
+	auto inputs_training = GenerateInput(nTrainingRecords, nFeatures, min, max);
+	auto inputs_validation = GenerateInput(nValidationRecords, nFeatures, min, max);
+	auto target_training = ComputeDeterminantTarget(inputs_training, nMatrixSize, nTrainingRecords);
+	auto target_validation = ComputeDeterminantTarget(inputs_validation, nMatrixSize, nValidationRecords);
+
+	//data is ready, we start training
+	clock_t start_application = clock();
+	clock_t current_time = clock();
+
+	std::vector<double> xmin;
+	std::vector<double> xmax;
+	Helper::FindMinMaxMatrix(xmin, xmax, inputs_training, nTrainingRecords, nFeatures);
+	double targetMin = Helper::Min(target_training, nTrainingRecords);
+	double targetMax = Helper::Max(target_training, nTrainingRecords);
+
+	int nU0 = 128;
+	int nU1 = 4;
+	int nU2 = 1;
+
+	std::vector<double> tmin0;
+	std::vector<double> tmax0;
+	for (int i = 0; i < nU0; ++i) {
+		tmin0.push_back(targetMin);
+		tmax0.push_back(targetMax);
+	}
+
+	std::vector<double> tmin1;
+	std::vector<double> tmax1;
+	for (int i = 0; i < nU1; ++i) {
+		tmin1.push_back(targetMin);
+		tmax1.push_back(targetMax);
+	}
+
+	std::vector<double> tmin2;
+	std::vector<double> tmax2;
+	for (int i = 0; i < nU2; ++i) {
+		tmin2.push_back(targetMin);
+		tmax2.push_back(targetMax);
+	}
+
+	auto layer0 = std::make_unique<Layer>(nU0, xmin, xmax, tmin0, tmax0, 3);
+	auto layer1 = std::make_unique<Layer>(nU1, tmin0, tmax0, tmin1, tmax1, 6);
+	auto layer2 = std::make_unique<Layer>(nU2, tmin1, tmax1, tmin2, tmax2, 12);
+
+	auto models0 = std::make_unique<double[]>(nU0);
+	auto models1 = std::make_unique<double[]>(nU1);
+	auto models2 = std::make_unique<double[]>(nU2);
+	auto deltas0 = std::make_unique<double[]>(nU0);
+	auto deltas1 = std::make_unique<double[]>(nU1);
+	auto deltas2 = std::make_unique<double[]>(nU2);
+
+	auto model_training = std::make_unique<double[]>(nTrainingRecords);
+	auto model_validation = std::make_unique<double[]>(nValidationRecords);
+	for (int epoch = 0; epoch < 12; ++epoch) {
+		for (int i = 0; i < nTrainingRecords; ++i) {
+			layer0->Input2Output(inputs_training[i], models0);
+			layer1->Input2Output(models0, models1);
+			layer2->Input2Output(models1, models2);
+			deltas2[0] = target_training[i] - models2[0];
+			model_training[i] = models2[0];
+			layer2->GetDeltas(deltas2, deltas1);
+			layer1->GetDeltas(deltas1, deltas0);
+			layer2->UpdateLayerUsingMemory(deltas2, 0.01);
+			layer1->UpdateLayerUsingMemory(deltas1, 0.05);
+			layer0->UpdateLayerUsingMemory(deltas0, 0.2);
+		}
+		if (epoch >= 0) {
+			for (int i = 0; i < nValidationRecords; ++i) {
+				layer0->Input2Output(inputs_validation[i], models0, true);
+				layer1->Input2Output(models0, models1, true);
+				layer2->Input2Output(models1, models2, true);
+				model_validation[i] = models2[0];
+			}
+			double training_pearson = Helper::Pearson(model_training, target_training, nTrainingRecords);
+			double validation_pearson =
+				Helper::Pearson(model_validation, target_validation, nValidationRecords);
+			current_time = clock();
+			printf("E %d, training %6.3f, validation %6.3f, time %2.3f\n",
+				epoch + 1, training_pearson, validation_pearson, 
+				(double)(current_time - start_application) / CLOCKS_PER_SEC);
+		}
+	}
+}
+
 void Triangulars_2_Layers() {
+	printf("Target is areas of triangulars given by coordinates of vertices, model is classic KAR\n");
 	int nFeatures = 6;
 	int nTrainingRecords = 10000;
 	int nValidationRecords = 2000;
@@ -119,6 +272,7 @@ void Triangulars_2_Layers() {
 }
 
 void Triangulars_3_Layers() {
+	printf("Target is areas of triangulars given by coordinates of vertices, model 3 layer KAN\n");
 	int nFeatures = 6;
 	int nTrainingRecords = 10000;
 	int nValidationRecords = 2000;
@@ -214,5 +368,6 @@ int main()
 	srand((unsigned int)time(NULL));
 	Triangulars_2_Layers();
 	Triangulars_3_Layers();
+	Determinant_4_4_3Layers();
 }
 
